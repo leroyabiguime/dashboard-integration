420135d17b18e72a82155078b8958eafefebdc4f
Leroy Mataa-Hewa Abiguime <mataa.abiguime17@gmail.com>
2022-05-26 20:48:27 +0000

    update json

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 
 import Dashboard from '../pages/Dashboard'
-import Customers from '../pages/Customers'
 import Categories from '../pages/Categories'
 import Products from '../pages/Products'
 import Analytics from '../pages/Analytics'
@@ -12,7 +11,6 @@ const RoutesList = () => {
   return (
       <Routes>
         <Route path="/" exact element={<Dashboard/>} />
-        <Route path="/customers" exact  element={<Customers/>}/>
         <Route path="/categories" exact  element={<Categories/>}/>
         <Route path="/products" exact  element={<Products/>}/>
         <Route path="/orders" exact  element={<Orders/>}/>
