87eb509689fff953dec61520a48bbe0edb9e10b9
Leroy Mataa-Hewa Abiguime <mataa.abiguime17@gmail.com>
2022-05-26 21:12:45 +0000

    update stye

--- src/components/topnav/Topnav.jsx
+++ src/components/topnav/Topnav.jsx
@@ -9,7 +9,7 @@ import user_menu from '../../assets/JsonData/user_menus.json'
 import user_image from '../../assets/images/logo.png'
 import ThemeMenu from '../thememenu/ThemeMenu'
 const curr_user = {
-    display_name: 'Leroy Abiguime',
+    display_name: 'leroyabiguime',
     image: user_image
 }
 
@@ -61,7 +61,7 @@ const Topnav = () => {
                 <div className="topnav__right-item">
                 <Dropdown
                 icon='bx bx-bell'
-                badge='12'
+                badge='30'
                 contentData={notifications}
                 renderItems={(item, index) => renderNotificationItem(item, index)}
                 renderFooter={() => <Link to='/'>View All </Link>}
